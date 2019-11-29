701f9b96c3a0f5d2c2550040f9c03dba313dd6a6
Maicon Domingues <maikonbdomingues@gmail.com>
2019-11-29 07:40:48 -0300

    Configure settings profile user at Session, Schedule and User Controllers

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 import Youch from 'youch';
 import * as Sentry from '@sentry/node';
 import 'express-async-errors';
@@ -23,6 +24,9 @@ class App {
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
+    // restringe o acesso a API de apenas um endereço, exemplo em producao:
+    // this.server.use(cors({ origin: 'https://rocketseat.com.br' }));
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
